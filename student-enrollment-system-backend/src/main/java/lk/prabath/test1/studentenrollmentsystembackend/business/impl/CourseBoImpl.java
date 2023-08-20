62e0d090497feb0748b3f247b9f6b1ee23d5aed9
prabath sanjeewa <prabathslakmal@gmail.com>
2023-08-20 12:24:22 +0530

    Complete the CourseBoImpl

--- student-enrollment-system-backend/src/main/java/lk/prabath/test1/studentenrollmentsystembackend/business/impl/CourseBoImpl.java
+++ student-enrollment-system-backend/src/main/java/lk/prabath/test1/studentenrollmentsystembackend/business/impl/CourseBoImpl.java
@@ -1,34 +1,45 @@
 package lk.prabath.test1.studentenrollmentsystembackend.business.impl;
 
 import lk.prabath.test1.studentenrollmentsystembackend.business.CourseBo;
+import lk.prabath.test1.studentenrollmentsystembackend.business.util.Transformer;
+import lk.prabath.test1.studentenrollmentsystembackend.dao.CourseDao;
 import lk.prabath.test1.studentenrollmentsystembackend.dto.CourseDto;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CourseBoImpl implements CourseBo {
+    private final CourseDao courseDao;
+    private final Transformer transformer;
+
+    public CourseBoImpl(CourseDao courseDao, Transformer transformer) {
+        this.courseDao = courseDao;
+        this.transformer = transformer;
+    }
+
     @Override
     public CourseDto saveCourse(CourseDto course) {
-        return null;
+        return transformer.fromCourseEntity(courseDao.save(transformer.toCourseEntity(course)));
     }
 
     @Override
     public void updateCourse(CourseDto course) {
-
+        courseDao.update(transformer.toCourseEntity(course));
     }
 
     @Override
     public void deleteCourse(String courseId) {
-
+        courseDao.deleteById(courseId);
     }
 
     @Override
     public Optional<CourseDto> findCourseById(String courseId) {
-        return Optional.empty();
+        return Optional.ofNullable(transformer.fromCourseEntity(courseDao.findById(courseId).get()));
     }
 
     @Override
     public List<CourseDto> findAllCourse() {
-        return null;
+        return courseDao.findAll().stream().map(transformer::fromCourseEntity).collect(Collectors.toList());
     }
 }
